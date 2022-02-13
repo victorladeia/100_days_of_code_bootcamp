f79016a5182aa90d4073d2b8826ce9c20c5737d9
Victor Ladeia <ladeiavictor@gmail.com>
2022-02-12 22:43:23 -0300

    Correction on Banker Roulette exercise of Day 4

--- Day_4/Banker_Roulette/main.py
+++ Day_4/Banker_Roulette/main.py
@@ -3,4 +3,4 @@ import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
-print(f"{names[random.randint(0, len(names))]} is going to buy the meal today!")
+print(f"{names[random.randint(0, (len(names) - 1))]} is going to buy the meal today!")
