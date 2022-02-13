e9db7d389683dd2beb9865f7b67f016fdf56425b
Victor Ladeia <ladeiavictor@gmail.com>
2022-02-13 11:54:16 -0300

    Improve of game from day 4.

--- Day_4/Final_Project-Rock_Paper_Scissors/main.py
+++ Day_4/Final_Project-Rock_Paper_Scissors/main.py
@@ -5,25 +5,28 @@ person_choice = int( input("What do you choose? Type 0 for Rock, 1 for Paper or 
 
 computer_choice = random.randint(0, 2)
 
-if ( person_choice == 0 ):
-    print( game_hands.rock )
-elif ( person_choice == 1 ):
-    print( game_hands.paper )
-elif ( person_choice == 2 ):
-    print( game_hands.scissors )
-
-print( "Computer chose: " )
-
-if ( computer_choice == 0 ):
-    print( game_hands.rock )
-elif ( computer_choice == 1 ):
-    print( game_hands.paper )
-elif ( computer_choice == 2 ):
-    print( game_hands.scissors )
-
-if ( ( person_choice == 0 and computer_choice == 2 ) or (person_choice == 1 and computer_choice == 0) or (person_choice == 2 and computer_choice == 1)):
-    print("You win!")
-elif ( ( computer_choice == 0 and person_choice == 2 ) or (computer_choice == 1 and person_choice == 0) or (computer_choice == 2 and person_choice == 1)):
-    print("You lose!")
+if (person_choice >= 3 or person_choice < 0):
+    print("Invalid Number. You lost.")
 else:
-    print("It's a Tie!")
+    if ( person_choice == 0 ):
+        print( game_hands.rock )
+    elif ( person_choice == 1 ):
+        print( game_hands.paper )
+    elif ( person_choice == 2 ):
+        print( game_hands.scissors )
+
+    print( "Computer chose: " )
+
+    if ( computer_choice == 0 ):
+        print( game_hands.rock )
+    elif ( computer_choice == 1 ):
+        print( game_hands.paper )
+    elif ( computer_choice == 2 ):
+        print( game_hands.scissors )
+
+    if ( ( person_choice == 0 and computer_choice == 2 ) or (person_choice == 1 and computer_choice == 0) or (person_choice == 2 and computer_choice == 1)):
+        print("You win!")
+    elif ( ( computer_choice == 0 and person_choice == 2 ) or (computer_choice == 1 and person_choice == 0) or (computer_choice == 2 and person_choice == 1)):
+        print("You lose!")
+    else:
+        print("It's a Tie!")
