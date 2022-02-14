c9c9f43d5e84d2890c4193804cc5c9edc3392c3c
Victor Ladeia <ladeiavictor@gmail.com>
2022-02-14 13:22:31 -0300

    Updates in examples file of Day 5
    - Create examples about for loops with range() e step using.

--- Day_5/examples.py
+++ Day_5/examples.py
@@ -1,3 +1,20 @@
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
+
+# for loop with lists
+
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+
+
+# for loop with range()
+
+# for number in range(1, 10):
+#     print(number)
+
+# for number in range(0, 13, 3):
+#     print(number)
+
+total = 0
+for num in range(1, 101):
+    total += num
+print(total)
