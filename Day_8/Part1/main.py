eeae4fd54e062306756730bddbcaead515904bd0
Victor Ladeia <ladeiavictor@gmail.com>
2022-02-27 16:19:06 -0300

    First commit of Day 8
    - Paint Area Calculator Done!
    - Added some code as examples.

---
+++ Day_8/Part1/main.py
@@ -0,0 +1,19 @@
+# def greet():
+#     print( "Hello, Victor!" )
+#     print( "Hello, Victor!" )
+#     print( "Hello, Victor!" )
+
+# greet()
+
+# def greet_with_name(name):
+#     print( f"Hello, {name}" )
+
+# greet_with_name("Victor")
+
+## Functions with more than 1 input:
+
+def greet_with(name, location):
+    print( f"My name is {name}." )
+    print( f"I'm from {location}." )
+
+greet_with(location="Brazil", name="Victor")
