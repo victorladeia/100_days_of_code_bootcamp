cff871dfd1c282d4d9f251913f269c7d9140bc86
ladeiavictor <ladeiavictor@gmail.com>
2022-03-29 19:12:44 -0300

    1st commit of Day 9

--- Day_9/main.py
+++ Day_9/main.py
@@ -1,1 +1,31 @@
-print("hello, world!")
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action os doing something over and over again.",
+}
+
+#Retriving items from dictionary.
+#print(programming_dictionary["Bug"])
+
+#Adding new items to dictionary
+programming_dictionary["Print()"] = "Prints in the command line."
+
+#print(programming_dictionary)
+
+#Creating an empty dictionary
+empty_dictionary = {}
+
+# wipe an existing dictionary
+
+#programming_dictionary = {}
+#print(programming_dictionary)
+
+# Edit a item in a dictionary
+#programming_dictionary["Bug"] = "A moth in your computer."
+#print(programming_dictionary)
+
+# Loop through a dictionary
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
