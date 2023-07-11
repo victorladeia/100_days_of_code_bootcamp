53ff992965902419f9c0a6bd3f32a9b5f9739656
victorladeia <ladeiavictor@gmail.com>
2023-07-11 06:08:57 -0300

    Adding files to Day 1

---
+++ Day_1/Day-1-4-Exercise/main.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below
+a = input( "a: " )
+b = input( "b: " )
+
+c = a
+a = b
+b = c
+
+print( "a = " + a )
+print( "b = " + b )
---
+++ Day_1/Final_Project-Name_Generator/main.py
@@ -0,0 +1,17 @@
+#1. Creating a Greeting for my program:
+
+print( "Welcome to the Band Name Generator!" )
+
+#2. Ask the user for the city they grew up in:
+
+city_name = input( "What's the name of the city you grew up in?\n" )
+
+#3. Ask the user for the name of a pet:
+
+pet_name = input( "What's your pet's name?\n" )
+
+#4. Combine the name of thei city and pet and show them their band name.
+
+band_name = city_name + " " + pet_name
+
+print( "Your band name could be " + band_name + "." )
