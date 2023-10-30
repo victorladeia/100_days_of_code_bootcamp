d7aa415655f6ff396207e3dac091457188328b71
victorladeia <ladeiavictor@gmail.com>
2023-10-30 05:11:13 -0300

    Day 20 remade.

---
+++ Rework for Learning/Day 20/main.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+from time import sleep
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+# Getting user directions:
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    sleep(0.1)
+    snake.move()
+
+    # Detecting collision with food:
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detecting collision with the wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detecting collision with the tail:
+    for block in snake.snake_blocks[1:]:
+        if snake.head.distance(block) < 10:
+            game_is_on = False
+            scoreboard.game_over()
---
+++ Rework for Learning/Day 20/snake.py
@@ -0,0 +1,57 @@
+from turtle import Turtle
+
+# setting angles values for set directions on setheading() function
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+# Setting starting positions for first 3 snake blocks:
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake_blocks = []
+        self.create_snake()
+        self.head = self.snake_blocks[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_block(position)
+
+    def add_block(self, position):
+        snake_block = Turtle()
+        snake_block.shape("square")
+        snake_block.color("white")
+        snake_block.penup()
+        snake_block.setposition(position)
+        self.snake_blocks.append(snake_block)
+
+    def extend(self):
+        self.add_block(self.snake_blocks[-1].position())
+
+    def move(self):
+
+        for part_num in range(len(self.snake_blocks) - 1, 0, -1):
+            new_x = self.snake_blocks[part_num - 1].xcor()
+            new_y = self.snake_blocks[part_num - 1].ycor()
+            self.snake_blocks[part_num].goto(new_x, new_y)
+        self.snake_blocks[0].forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
